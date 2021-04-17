ffe4f39659597db391102d3b08325dad75a94597
Gabriel Rodrigues <jipacoding@gmail.com>
2021-04-17 17:28:02 -0400

    change: scripts: simplify scripts with `s:` prefix

--- scripts.sh
+++ scripts.sh
@@ -1,36 +1,22 @@
-function scripts-usage() {
-  cat <<\EOF
-Usage:
-$ scripts-setup
-$ scripts-build
-$ scripts-format
-$ scripts-start [project] [executable]
-$ scripts-build-and-start [project] [executable]
-EOF
-}
-
-if [ -z ${HIDE_HELP+x} ]; then scripts-usage; fi;
-
-
-function scripts-setup() {
+function s:setup() {
   rm -rf builddir;
   meson setup builddir;
 }
 
-function scripts-start() {
-  ./builddir/$1/$2
+function s:build-start() {
+  clear;
+  s:build && clear && s:start $1 $2;
 }
 
-function scripts-build() {
+function s:build() {
   meson compile -C builddir;
 }
 
-function scripts-build-and-start() {
-  clear;
-  scripts-build && clear && scripts-start $1 $2;
+function s:start() {
+  ./builddir/$1/$2
 }
 
-function scripts-format() {
+function s:format() {
   if ! command -v COMMAND &> /dev/null
   then
     echo "uncrustify command not found - skipping staged files format"
