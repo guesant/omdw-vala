f37aaffcb5b31927b4d3a792e05e290991155c79
Gabriel Rodrigues <jipacoding@gmail.com>
2021-04-17 17:54:00 -0400

    hooks: fix format script usage

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -3,7 +3,7 @@
 CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACM -- '*.vala')
 
 if \[ -n "$CHANGED_FILES" \]; then
-    HIDE_HELP=true source scripts.sh;
-    scripts-format;
+    source scripts.sh;
+    s:format;
     git add $CHANGED_FILES;
 fi
